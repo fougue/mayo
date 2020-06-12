4858c61be7c7fc73be6bc6283cf5d1c8aaf1ffd3
Hugues Delorme <delorme.hugues@fougue.pro>
2020-06-13 01:26:54 +0200

    Base: introduce Messenger

---
+++ src/base/messenger.h
@@ -0,0 +1,45 @@
+/****************************************************************************
+** Copyright (c) 2020, Fougue Ltd. <http://www.fougue.pro>
+** All rights reserved.
+** See license at https://github.com/fougue/mayo/blob/master/LICENSE.txt
+****************************************************************************/
+
+#pragma once
+
+#include <QtCore/QObject>
+
+namespace Mayo {
+
+class Messenger : public QObject {
+    Q_OBJECT
+public:
+    Messenger(QObject* parent = nullptr);
+    static Messenger* defaultInstance();
+
+    enum class MessageType {
+        Trace,
+        Info,
+        Warning,
+        Error
+    };
+
+    void emitTrace(const QString& text);
+    void emitInfo(const QString& text);
+    void emitWarning(const QString& text);
+    void emitError(const QString& text);
+    virtual void emitMessage(MessageType msgType, const QString& text);
+
+signals:
+    void message(MessageType msgType, const QString& text);
+};
+
+class NullMessenger : public Messenger {
+public:
+    static NullMessenger* instance();
+    void emitMessage(MessageType msgType, const QString& text) override;
+
+private:
+    NullMessenger() = default;
+};
+
+} // namespace Mayo
