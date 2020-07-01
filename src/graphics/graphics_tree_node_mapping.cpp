192e9650e088a3f1804a11ea8b4d2cc6b95cdc9f
Hugues Delorme <delorme.hugues@fougue.pro>
2020-07-01 13:04:28 +0200

    Gui: support graphics highlighting also fore pure mesh XCAF models

--- src/graphics/graphics_tree_node_mapping.cpp
+++ src/graphics/graphics_tree_node_mapping.cpp
@@ -16,9 +16,14 @@
 
 namespace Mayo {
 
+GraphicsShapeTreeNodeMapping::GraphicsShapeTreeNodeMapping(TopAbs_ShapeEnum shapeType)
+    : m_shapeType(shapeType)
+{
+}
+
 int GraphicsShapeTreeNodeMapping::selectionMode() const
 {
-    return AIS_Shape::SelectionMode(TopAbs_SOLID);
+    return AIS_Shape::SelectionMode(m_shapeType);
 }
 
 std::vector<GraphicsOwnerPtr>
@@ -26,18 +31,18 @@ GraphicsShapeTreeNodeMapping::findGraphicsOwners(const DocumentTreeNode& treeNod
 {
     const TopLoc_Location shapeLoc = treeNode.document()->xcaf().shapeAbsoluteLocation(treeNode.id());
     const TopoDS_Shape shape = XCaf::shape(treeNode.label()).Located(shapeLoc);
-    std::vector<TopoDS_Shape> vecSolid;
-    if (BRepUtils::moreComplex(shape.ShapeType(), TopAbs_SOLID)) {
-        BRepUtils::forEachSubShape(shape, TopAbs_SOLID, [&](const TopoDS_Shape& solid) {
-            vecSolid.push_back(solid);
+    std::vector<TopoDS_Shape> vecShape;
+    if (BRepUtils::moreComplex(shape.ShapeType(), m_shapeType)) {
+        BRepUtils::forEachSubShape(shape, m_shapeType, [&](const TopoDS_Shape& subShape) {
+            vecShape.push_back(subShape);
         });
     }
-    else if (shape.ShapeType() == TopAbs_SOLID) {
-        vecSolid.push_back(TopoDS::Solid(shape));
+    else if (shape.ShapeType() == m_shapeType) {
+        vecShape.push_back(shape);
     }
 
     std::vector<GraphicsOwnerPtr> vecGfxOwner;
-    for (const TopoDS_Shape& shape : vecSolid) {
+    for (const TopoDS_Shape& shape : vecShape) {
         auto it = m_mapGfxOwner.find(BRepUtils::hashCode(shape));
         if (it != m_mapGfxOwner.cend())
             vecGfxOwner.push_back(it->second);
@@ -52,18 +57,38 @@ bool GraphicsShapeTreeNodeMapping::mapGraphicsOwner(const GraphicsOwnerPtr& gfxO
     if (brepOwner.IsNull())
         return false;
 
+    if (brepOwner->Shape().ShapeType() != m_shapeType)
+        return false;
+
     auto result = m_mapGfxOwner.emplace(BRepUtils::hashCode(brepOwner->Shape()), brepOwner);
     return result.second;
 }
 
-bool GraphicsShapeTreeNodeMappingDriver::supportsEntity(const TDF_Label& label) const
+std::unique_ptr<GraphicsTreeNodeMapping>
+GraphicsShapeTreeNodeMappingDriver::createMapping(const DocumentTreeNode& entityTreeNode) const
 {
-    return XCaf::isShape(label);
-}
+    if (!entityTreeNode.isEntity())
+        return {};
 
-std::unique_ptr<GraphicsTreeNodeMapping> GraphicsShapeTreeNodeMappingDriver::createMapping() const
-{
-    return std::make_unique<GraphicsShapeTreeNodeMapping>();
+    if (!entityTreeNode.isValid())
+        return {};
+
+    if (!XCaf::isShape(entityTreeNode.label()))
+        return {};
+
+    int solidCount = 0;
+    int faceCount = 0;
+    const DocumentPtr doc = entityTreeNode.document();
+    deepForeachTreeNode(entityTreeNode.id(), doc->modelTree(), [&](TreeNodeId treeNodeId) {
+        const TopoDS_Shape shape = XCaf::shape(doc->modelTree().nodeData(treeNodeId));
+        if (shape.ShapeType() == TopAbs_SOLID)
+            ++solidCount;
+        else if (shape.ShapeType() == TopAbs_FACE)
+            ++faceCount;
+    });
+
+    const TopAbs_ShapeEnum shapeType = solidCount > faceCount ? TopAbs_SOLID : TopAbs_FACE;
+    return std::make_unique<GraphicsShapeTreeNodeMapping>(shapeType);
 }
 
 } // namespace Mayo
