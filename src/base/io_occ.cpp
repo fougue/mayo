bc00b3a154ee944a931843f04a7046e69988bded
Hugues Delorme <delorme.hugues@fougue.pro>
2020-12-11 10:13:29 +0100

    Base: simplify declaration of IO readers/writers in OCC factory

--- src/base/io_occ.cpp
+++ src/base/io_occ.cpp
@@ -60,7 +60,7 @@ public:
     using ExchangerGenerator = Generator<Format, std::unique_ptr<EXCHANGER>>;
     using PropertiesGenerator = Generator<Format, std::unique_ptr<PropertyGroup>, PropertyGroup*>;
 
-    void addExchanger(
+    FactoryHelper& addExchanger(
             const Format& format,
             const typename ExchangerGenerator::FunctionType& fnExchanger,
             const PropertiesGenerator::FunctionType& fnProperties)
@@ -69,13 +69,20 @@ public:
         m_vecExchangerGenerator.push_back({ format, fnExchanger });
         if (fnProperties)
             m_vecPropertiesGenerator.push_back({ format, fnProperties });
+
+        return *this;
     }
 
-    template<typename T> void addExchanger(const Format& format) {
-        this->addExchanger(format, &createProduct<T>, &T::createProperties);
+    template<typename T> FactoryHelper& addExchanger(const Format& format) {
+        static_assert(std::is_base_of<EXCHANGER, T>::value);
+        if constexpr(HasMember_createProperties<T>::value) {
+            return this->addExchanger(format, &createProduct<T>, &T::createProperties);
+        }
+        else {
+            return this->addExchanger(format, &createProduct<T>, nullptr);
+        }
     }
 
-    bool empty() const { return m_vecFormat.empty(); }
     Span<const Format> formats() const { return m_vecFormat; }
 
     typename ExchangerGenerator::ProductType createExchanger(const Format& format) const {
@@ -89,6 +96,17 @@ public:
     }
 
 private:
+    template<typename T> class HasMember_createProperties {
+        using Yes = char[2];
+        using No = char[1];
+        struct Fallback { int createProperties; };
+        struct Derived : T, Fallback {};
+        template<typename U> static No& test(decltype(U::createProperties)*);
+        template<typename U> static Yes& test(U*);
+    public:
+        static constexpr bool value = sizeof(test<Derived>(nullptr)) == sizeof(Yes);
+    };
+
     template<typename GENERATOR>
     static const GENERATOR& findGenerator(
             const typename GENERATOR::KeyType& key, Span<const GENERATOR> spanGenerator)
@@ -108,35 +126,29 @@ private:
 
 const FactoryHelper<Reader>& helper_OccFactoryReader()
 {
-    static FactoryHelper<Reader> helper;
-    if (helper.empty()) {
-        helper.addExchanger<OccStepReader>(Format_STEP);
-        helper.addExchanger<OccIgesReader>(Format_IGES);
-        helper.addExchanger(Format_OCCBREP, &createProduct<OccBRepReader>, nullptr);
-#if OCC_VERSION_HEX >= OCC_VERSION_CHECK(7, 4, 0)
-        helper.addExchanger<OccGltfReader>(Format_GLTF);
-        helper.addExchanger<OccObjReader>(Format_OBJ);
-#endif
-        helper.addExchanger(Format_STL, &createProduct<OccStlReader>, nullptr);
-    }
-
+    static auto helper = FactoryHelper<Reader>()
+            .addExchanger<OccStepReader>(Format_STEP)
+            .addExchanger<OccIgesReader>(Format_IGES)
+            .addExchanger<OccBRepReader>(Format_OCCBREP)
+        #if OCC_VERSION_HEX >= OCC_VERSION_CHECK(7, 4, 0)
+            .addExchanger<OccGltfReader>(Format_GLTF)
+            .addExchanger<OccObjReader>(Format_OBJ)
+        #endif
+            .addExchanger<OccStlReader>(Format_STL);
     return helper;
 }
 
 const FactoryHelper<Writer>& helper_OccFactoryWriter()
 {
-    static FactoryHelper<Writer> helper;
-    if (helper.empty()) {
-        helper.addExchanger<OccStepWriter>(Format_STEP);
-        helper.addExchanger<OccIgesWriter>(Format_IGES);
-        helper.addExchanger(Format_OCCBREP, &createProduct<OccBRepWriter>, nullptr);
-#if OCC_VERSION_HEX >= OCC_VERSION_CHECK(7, 5, 0)
-        helper.addExchanger<OccGltfWriter>(Format_GLTF);
-#endif
-        helper.addExchanger<OccStlWriter>(Format_STL);
-        helper.addExchanger<OccVrmlWriter>(Format_VRML);
-    }
-
+    static auto helper = FactoryHelper<Writer>()
+            .addExchanger<OccStepWriter>(Format_STEP)
+            .addExchanger<OccIgesWriter>(Format_IGES)
+            .addExchanger<OccBRepWriter>(Format_OCCBREP)
+        #if OCC_VERSION_HEX >= OCC_VERSION_CHECK(7, 5, 0)
+            .addExchanger<OccGltfWriter>(Format_GLTF)
+        #endif
+            .addExchanger<OccStlWriter>(Format_STL)
+            .addExchanger<OccVrmlWriter>(Format_VRML);
     return helper;
 }
 
