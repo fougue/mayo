ba55f7230af356648a2418eade91c8970d5d8dab
Hugues Delorme <delorme.hugues@fougue.pro>
2021-02-03 16:23:08 +0100

    Scripts: process appveyor.yml in bump-version.rb

--- scripts/bump-version.rb
+++ scripts/bump-version.rb
@@ -1,5 +1,5 @@
 ##
-## Copyright (c) 2019, Fougue Ltd. <http://www.fougue.pro>
+## Copyright (c) 2021, Fougue Ltd. <http://www.fougue.pro>
 ## All rights reserved.
 ## See license at https://github.com/fougue/mayo/blob/master/LICENSE.txt
 ##
@@ -34,3 +34,10 @@ version_pri.sub!(/(MAYO_VERSION_MIN\s*=\s*)\d+/, "\\1#{minor}")
 version_pri.sub!(/(MAYO_VERSION_PAT\s*=\s*)\d+/, "\\1#{patch}")
 File.open(path_version_pri, "w").write(version_pri)
 puts "Bumped #{path_version_pri}"
+
+# ../appveyor.yml
+path_appveyor_yml = "#{script_dir_name}/../appveyor.yml"
+appveyor_yml = File.open(path_appveyor_yml, "r").read
+appveyor_yml.sub!(/(version\s*:\s*)\d+\.\d+/, "\\1#{major}.#{minor}")
+File.open(path_appveyor_yml, "w").write(appveyor_yml)
+puts "Bumped #{path_appveyor_yml}"
