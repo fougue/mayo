9bac58e02737d01d996878da4497688be439e62e
Hugues Delorme <delorme.hugues@fougue.pro>
2021-03-25 10:41:44 +0100

    App: fix 'inconsistent-missing-override' clang warnings in occt_window_7xx.h files

--- src/app/occt_window_750.h
+++ src/app/occt_window_750.h
@@ -52,38 +52,38 @@ public:
   }
 
   //! Returns native Window handle
-  virtual Aspect_Drawable NativeHandle() const;
+  Aspect_Drawable NativeHandle() const override;
 
   //! Returns parent of native Window handle.
-  virtual Aspect_Drawable NativeParentHandle() const;
+  Aspect_Drawable NativeParentHandle() const override;
 
   //! Applies the resizing to the window <me>
-  virtual Aspect_TypeOfResize DoResize();
+  Aspect_TypeOfResize DoResize() override;
 
   //! Returns True if the window <me> is opened
   //! and False if the window is closed.
-  virtual Standard_Boolean IsMapped() const;
+  Standard_Boolean IsMapped() const override;
 
   //! Apply the mapping change to the window <me>
   //! and returns TRUE if the window is mapped at screen.
-  virtual Standard_Boolean DoMapping() const { return Standard_True; }
+  Standard_Boolean DoMapping() const override { return Standard_True; }
 
   //! Opens the window <me>.
-  virtual void Map() const;
+  void Map() const override;
 
   //! Closes the window <me>.
-  virtual void Unmap() const;
+  void Unmap() const override;
 
-  virtual void Position( Standard_Integer& theX1, Standard_Integer& theY1,
-                         Standard_Integer& theX2, Standard_Integer& theY2 ) const;
+  void Position( Standard_Integer& theX1, Standard_Integer& theY1,
+                 Standard_Integer& theX2, Standard_Integer& theY2 ) const override;
 
   //! Returns The Window RATIO equal to the physical
   //! WIDTH/HEIGHT dimensions.
-  virtual Standard_Real Ratio() const;
+  Standard_Real Ratio() const override;
 
-  virtual void Size( Standard_Integer& theWidth, Standard_Integer& theHeight ) const;
+  void Size( Standard_Integer& theWidth, Standard_Integer& theHeight ) const override;
 
-  virtual Aspect_FBConfig NativeFBConfig() const Standard_OVERRIDE { return NULL; }
+  Aspect_FBConfig NativeFBConfig() const override { return NULL; }
 
   DEFINE_STANDARD_RTTIEXT(OcctWindow,Aspect_Window)
 
