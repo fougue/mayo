01897e7d7e6dcf95712c1d5be883d7778dbd52fb
Hakan Kaya <mail@hakankaya.kim>
2021-08-05 13:40:46 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/CMakeFiles/mayo_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/mayo_autogen"
-  "mayo_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/mayo_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
