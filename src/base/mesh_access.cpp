6deaa6038aa95bafda6f3123725ec6ba8ad84e13
Hugues Delorme <delorme.hugues@fougue.pro>
2022-05-16 18:59:24 +0200

    Base: add IMeshAccess utility

---
+++ src/base/mesh_access.cpp
@@ -0,0 +1,103 @@
+/****************************************************************************
+** Copyright (c) 2022, Fougue Ltd. <http://www.fougue.pro>
+** All rights reserved.
+** See license at https://github.com/fougue/mayo/blob/master/LICENSE.txt
+****************************************************************************/
+
+#include "mesh_access.h"
+
+#include "brep_utils.h"
+#include "caf_utils.h"
+#include "data_triangulation.h"
+#include "document.h"
+#include "document_tree_node.h"
+
+#include <BRep_Tool.hxx>
+#include <Standard_Version.hxx>
+#include <TopoDS_Face.hxx>
+#if OCC_VERSION_HEX >= 0x070500
+#  include <XCAFDoc_VisMaterial.hxx>
+#endif
+
+namespace Mayo {
+
+// Provides mesh access to a TopoDS_Face object stored within XCAF document
+class XCafFace_MeshAccess : public IMeshAccess {
+public:
+    XCafFace_MeshAccess(const DocumentTreeNode& treeNode, const TopoDS_Face& face)
+    {
+        const DocumentPtr& doc = treeNode.document();
+        const TDF_Label labelNode = treeNode.label();
+        TDF_Label labelShape = labelNode;
+        if (doc->xcaf().isShapeSubOf(labelNode, face))
+            labelShape = doc->xcaf().findShapeLabel(face);
+
+        if (doc->xcaf().hasShapeColor(labelShape)) {
+            m_faceColor = doc->xcaf().shapeColor(labelShape);
+            //const Quantity_Color sc{ c.Red(), c.Green(), c.Blue(), Quantity_TOC_sRGB };
+        }
+        else {
+#if OCC_VERSION_HEX >= 0x070500
+            Handle_XCAFDoc_VisMaterialTool visMaterialTool = doc->xcaf().visMaterialTool();
+            Handle_XCAFDoc_VisMaterial visMaterial = visMaterialTool->GetShapeMaterial(labelShape);
+            if (visMaterial)
+                m_faceColor = visMaterial->BaseColor().GetRGB();
+#endif
+        }
+
+        const TopLoc_Location locShape = XCaf::shapeAbsoluteLocation(doc->modelTree(), treeNode.id());
+        TopLoc_Location locFace;
+        m_triangulation = BRep_Tool::Triangulation(face, locFace);
+        m_location = locShape * locFace;
+    }
+
+    Quantity_Color nodeColor(int /*i*/) const override { return m_faceColor; }
+    const TopLoc_Location& location() const override { return m_location; }
+    const Handle(Poly_Triangulation)& triangulation() const override { return m_triangulation; }
+
+private:
+    Quantity_Color m_faceColor;
+    TopLoc_Location m_location;
+    Handle(Poly_Triangulation) m_triangulation;
+};
+
+// Provides access to a mesh stored within a DataTriangulation attribute
+class DataTriangulation_MeshAccess : public IMeshAccess {
+public:
+    DataTriangulation_MeshAccess(const DocumentTreeNode& treeNode)
+        : m_dataTriangulation(CafUtils::findAttribute<DataTriangulation>(treeNode.label()))
+    {
+    }
+
+    Quantity_Color nodeColor(int i) const override
+    {
+        return m_dataTriangulation->nodeColors()[i];
+    }
+
+    const TopLoc_Location& location() const override { return m_location; }
+
+    const Handle(Poly_Triangulation)& triangulation() const override
+    {
+        return m_dataTriangulation->Get();
+    }
+
+private:
+    opencascade::handle<DataTriangulation> m_dataTriangulation;
+    TopLoc_Location m_location;
+};
+
+void IMeshAccess_visitMeshes(
+        const DocumentTreeNode& treeNode,
+        std::function<void(const IMeshAccess&)> fnCallback)
+{
+    if (XCaf::isShape(treeNode.label())) {
+        BRepUtils::forEachSubFace(XCaf::shape(treeNode.label()), [&](const TopoDS_Face& face) {
+            fnCallback(XCafFace_MeshAccess(treeNode, face));
+        });
+    }
+    else if (CafUtils::hasAttribute<DataTriangulation>(treeNode.label())) {
+        fnCallback(DataTriangulation_MeshAccess(treeNode));
+    }
+}
+
+} // namespace Mayo
---
+++ src/base/mesh_access.h
@@ -0,0 +1,37 @@
+/****************************************************************************
+** Copyright (c) 2022, Fougue Ltd. <http://www.fougue.pro>
+** All rights reserved.
+** See license at https://github.com/fougue/mayo/blob/master/LICENSE.txt
+****************************************************************************/
+
+#pragma once
+
+// Base
+class DocumentTreeNode;
+
+// OpenCascade
+#include <Quantity_Color.hxx>
+#include <Standard_Handle.hxx>
+class Poly_Triangulation;
+class TopLoc_Location;
+
+// CppStd
+#include <functional>
+
+namespace Mayo {
+
+// Provides an interface to access mesh geometry
+class IMeshAccess {
+public:
+    virtual Quantity_Color nodeColor(int i) const = 0;
+    virtual const TopLoc_Location& location() const = 0;
+    virtual const Handle(Poly_Triangulation)& triangulation() const = 0;
+};
+
+// Iterates over meshes from `treeNode` and call `fnCallback` for each item.
+void IMeshAccess_visitMeshes(
+        const DocumentTreeNode& treeNode,
+        std::function<void(const IMeshAccess&)> fnCallback
+);
+
+} // namespace Mayo
