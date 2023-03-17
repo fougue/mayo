96eb93764f8063ee03c6895cb6a18c0db7c8a06a
Hugues Delorme <delorme.hugues@fougue.pro>
2023-03-17 18:21:25 +0100

    Add CMake build script

---
+++ src/app/version.h.cmake
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace Mayo {
+
+const char strVersion[] = "@Mayo_VersionMajor@.@Mayo_VersionMinor@.@Mayo_VersionPatch@";
+const char strVersionCommitId[] = "@Mayo_VersionCommit@";
+const unsigned versionRevisionNumber = @Mayo_VersionRevNum@;
+const unsigned versionMajor = @Mayo_VersionMajor@;
+const unsigned versionMinor = @Mayo_VersionMinor@;
+const unsigned versionPatch = @Mayo_VersionPatch@;
+
+} // namespace Mayo
