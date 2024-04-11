a539521792d779087b1cf2df33c37637558ab853
Hugues Delorme <delorme.hugues@fougue.pro>
2024-04-11 10:22:10 +0200

    IO_OCC: fix weird "mirroring" after VRML loading

--- src/io_occ/io_occ_vrml_reader.cpp
+++ src/io_occ/io_occ_vrml_reader.cpp
@@ -12,6 +12,14 @@ namespace IO {
 OccVrmlReader::OccVrmlReader()
     : OccBaseMeshReader(m_reader)
 {
+    // Fixes weird "mirroring" of the loaded model
+    m_reader.SetFileLengthUnit(1.);
+#if 0
+    double scaleFactor = 1.;
+    if (!XCAFDoc_DocumentTool::GetLengthUnit(doc, scaleFactor))
+        scaleFactor = UnitsMethods::GetCasCadeLengthUnit();
+    m_reader.SetSystemLengthUnit(scaleFactor);
+#endif
 }
 
 std::unique_ptr<PropertyGroup> OccVrmlReader::createProperties(PropertyGroup* parentGroup)
