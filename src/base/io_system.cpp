d42d8929edbede879ad2325b5eefdfd7c74e6c30
Hugues Delorme <delorme.hugues@fougue.pro>
2026-08-24 19:09:54 +0200

    Base: split IO::System::importInDocument() into free functions

--- src/base/io_system.cpp
+++ src/base/io_system.cpp
@@ -5,15 +5,11 @@
 
 #include "io_system.h"
 
-#include "caf_utils.h"
 #include "document.h"
 #include "io_parameters_provider.h"
-#include "io_reader.h"
-#include "io_writer.h"
 #include "message_collecter.h"
 #include "task_manager.h"
 #include "task_progress.h"
-#include "tkernel_utils.h"
 
 #include <fmt/format.h>
 #include <gsl/util>
@@ -21,7 +17,6 @@
 #include <algorithm>
 #include <fstream>
 #include <locale>
-#include <mutex>
 #include <regex>
 #include <unordered_set>
 #include <vector>
@@ -48,6 +43,134 @@ void dispatchWarnings(std::string_view headerMsg, const MessageCollecter& msgCol
     const std::string strWarnings = msgCollect.asString("\n    ", MessageType::Warning);
     if (!strWarnings.empty())
         target->warning() << fmt::format("{}\n    {}", headerMsg, strWarnings);
+}
+
+bool isEntityPostProcessRequired(Format format, const System::Args_ImportInDocument& args)
+{
+    if (args.entityPostProcess && args.entityPostProcessRequiredIf)
+        return args.entityPostProcessRequiredIf(format);
+    else
+        return false;
+}
+
+struct ImportTaskData {
+    std::unique_ptr<Reader> reader;
+    FilePath filepath;
+    Format fileFormat = Format_Unknown;
+    TaskProgress* progress = nullptr;
+    TaskId taskId = 0;
+    NCollection_Sequence<TDF_Label> seqTransferredEntity;
+    bool readSuccess = false;
+    bool transferred = false; // Is transfer done ?
+    MessageCollecter messenger;
+};
+
+bool success(const ImportTaskData& taskData)
+{
+    return taskData.readSuccess && !taskData.seqTransferredEntity.IsEmpty();
+}
+
+void writeImportError(ImportTaskData& taskData, std::string_view errorMsg)
+{
+    taskData.messenger.error() << fmt::format(
+        System::textIdTr("Error during import of '{}'\n{}"), taskData.filepath.u8string(), errorMsg
+    );
+}
+
+void readFile(ImportTaskData& taskData, const System& ioSystem, const System::Args_ImportInDocument& args)
+{
+    auto error = [&](std::string_view trErrorMsg) {
+        writeImportError(taskData, trErrorMsg);
+        taskData.readSuccess = false;
+    };
+
+    taskData.fileFormat = ioSystem.probeFormat(taskData.filepath);
+    if (taskData.fileFormat == Format_Unknown)
+        return error(System::textIdTr("Unknown format"));
+
+    double portionSize = 40;
+    if (isEntityPostProcessRequired(taskData.fileFormat, args))
+        portionSize *= (100 - args.entityPostProcessProgressSize) / 100.;
+
+    TaskProgress progress(taskData.progress, portionSize, System::textIdTr("Reading file"));
+    taskData.reader = ioSystem.createReader(taskData.fileFormat);
+    if (!taskData.reader)
+        return error(System::textIdTr("No supporting reader"));
+
+    taskData.reader->setMessenger(&taskData.messenger);
+    if (args.parametersProvider) {
+        taskData.reader->applyProperties(
+            args.parametersProvider->findReaderParameters(taskData.fileFormat)
+        );
+    }
+
+    if (!taskData.reader->readFile(taskData.filepath, &progress))
+        return error(System::textIdTr("File read problem"));
+
+    taskData.readSuccess = true;
+}
+
+void transfer(ImportTaskData& taskData, const System::Args_ImportInDocument& args)
+{
+    if (!taskData.readSuccess)
+        return;
+
+    double portionSize = 60;
+    if (isEntityPostProcessRequired(taskData.fileFormat, args))
+        portionSize *= (100 - args.entityPostProcessProgressSize) / 100.;
+
+    TaskProgress progress(taskData.progress, portionSize, System::textIdTr("Transferring file"));
+    if (taskData.reader && !TaskProgress::isAbortRequested(&progress)) {
+        taskData.seqTransferredEntity = taskData.reader->transfer(args.targetDocument, &progress);
+        if (taskData.seqTransferredEntity.IsEmpty())
+            writeImportError(taskData, System::textIdTr("File transfer problem"));
+    }
+
+    taskData.transferred = true;
+}
+
+auto postProcess(ImportTaskData& taskData, const System::Args_ImportInDocument& args)
+{
+    if (!success(taskData))
+        return;
+
+    if (!isEntityPostProcessRequired(taskData.fileFormat, args))
+        return;
+
+    TaskProgress progress(
+        taskData.progress, args.entityPostProcessProgressSize, args.entityPostProcessProgressStep
+    );
+    const double subPortionSize = 100. / static_cast<double>(taskData.seqTransferredEntity.Size());
+    for (const TDF_Label& labelEntity : taskData.seqTransferredEntity) {
+        TaskProgress subProgress(&progress, subPortionSize);
+        args.entityPostProcess(labelEntity, &subProgress);
+    }
+}
+
+void addModelTreeEntities(const ImportTaskData& taskData, const DocumentPtr& targetDoc)
+{
+    if (!success(taskData))
+        return;
+
+    // Need to call Document::addEntityTreeNodeSequence() instead of addEntityTreeNode() in
+    // for() loop. The former function doesn't interleave update of the model tree and emission
+    // of "entity added" signal for each entity. This prevents data race to happen on the
+    // Document's model tree within slots connected to signal(and living in other threads)
+    targetDoc->addEntityTreeNodeSequence(taskData.seqTransferredEntity);
+}
+
+void dispatchMessages(ImportTaskData& taskData, Messenger* targetMessenger)
+{
+    const auto strFilepath = taskData.filepath.make_preferred().u8string();
+    dispatchWarnings(
+        fmt::format("Warning(s) during import from '{}'", strFilepath),
+        taskData.messenger, targetMessenger
+    );
+    dispatchErrors(
+        fmt::format("Errors(s) during import from '{}'", strFilepath),
+        taskData.messenger, targetMessenger
+    );
+    taskData.messenger.clear();
 }
 
 } // namespace
@@ -195,131 +318,25 @@ std::unique_ptr<Writer> System::createWriter(Format format) const
 
 bool System::importInDocument(const Args_ImportInDocument& args) const
 {
-    DocumentPtr doc = args.targetDocument;
-    const auto listFilepath = args.filepaths;
     TaskProgress* rootProgress = args.progress ? args.progress : &TaskProgress::null();
     Messenger* messenger = args.messenger ? args.messenger : &Messenger::null();
 
-    bool ok = true;
-
-    using ReaderPtr = std::unique_ptr<Reader>;
-    struct TaskData {
-        ReaderPtr reader;
-        FilePath filepath;
-        Format fileFormat = Format_Unknown;
-        TaskProgress* progress = nullptr;
-        TaskId taskId = 0;
-        NCollection_Sequence<TDF_Label> seqTransferredEntity;
-        bool readSuccess = false;
-        bool transferred = false;
-        MessageCollecter messenger;
-    };
-
-    auto fnEntityPostProcessRequired = [&](Format format) {
-        if (args.entityPostProcess && args.entityPostProcessRequiredIf)
-            return args.entityPostProcessRequiredIf(format);
-        else
-            return false;
-    };
-    auto fnAddError = [&](TaskData& taskData, std::string_view errorMsg) {
-        ok = false;
-        taskData.messenger.error() << fmt::format(
-            textIdTr("Error during import of '{}'\n{}"), taskData.filepath.u8string(), errorMsg
-        );
-    };
-    auto fnReadFileError = [&](TaskData& taskData, std::string_view errorMsg) {
-        fnAddError(taskData, errorMsg);
-        return false;
-    };
-    auto fnReadFile = [&](TaskData& taskData) {
-        taskData.fileFormat = this->probeFormat(taskData.filepath);
-        if (taskData.fileFormat == Format_Unknown)
-            return fnReadFileError(taskData, textIdTr("Unknown format"));
-
-        double portionSize = 40;
-        if (fnEntityPostProcessRequired(taskData.fileFormat))
-            portionSize *= (100 - args.entityPostProcessProgressSize) / 100.;
-
-        TaskProgress progress(taskData.progress, portionSize, textIdTr("Reading file"));
-        taskData.reader = this->createReader(taskData.fileFormat);
-        if (!taskData.reader)
-            return fnReadFileError(taskData, textIdTr("No supporting reader"));
-
-        taskData.reader->setMessenger(&taskData.messenger);
-        if (args.parametersProvider) {
-            taskData.reader->applyProperties(
-                args.parametersProvider->findReaderParameters(taskData.fileFormat)
-            );
-        }
-
-        if (!taskData.reader->readFile(taskData.filepath, &progress))
-            return fnReadFileError(taskData, textIdTr("File read problem"));
-
-        return true;
-    };
-    auto fnTransfer = [&](TaskData& taskData) {
-        double portionSize = 60;
-        if (fnEntityPostProcessRequired(taskData.fileFormat))
-            portionSize *= (100 - args.entityPostProcessProgressSize) / 100.;
-
-        TaskProgress progress(taskData.progress, portionSize, textIdTr("Transferring file"));
-        if (taskData.reader && !TaskProgress::isAbortRequested(&progress)) {
-            taskData.seqTransferredEntity = taskData.reader->transfer(doc, &progress);
-            if (taskData.seqTransferredEntity.IsEmpty())
-                fnAddError(taskData, textIdTr("File transfer problem"));
-        }
-
-        taskData.transferred = true;
-    };
-    auto fnPostProcess = [&](TaskData& taskData) {
-        if (!fnEntityPostProcessRequired(taskData.fileFormat))
-            return;
-
-        TaskProgress progress(
-            taskData.progress, args.entityPostProcessProgressSize, args.entityPostProcessProgressStep
-        );
-        const double subPortionSize = 100. / double(taskData.seqTransferredEntity.Size());
-        for (const TDF_Label& labelEntity : taskData.seqTransferredEntity) {
-            TaskProgress subProgress(&progress, subPortionSize);
-            args.entityPostProcess(labelEntity, &subProgress);
-        }
-    };
-    auto fnAddModelTreeEntities = [&](const TaskData& taskData) {
-        // Need to call Document::addEntityTreeNodeSequence() instead of addEntityTreeNode() in
-        // for() loop. The former function doesn't interleave update of the model tree and emission
-        // of "entity added" signal for each entity. This prevents data race to happen on the
-        // Document's model tree within slots connected to signal(and living in other threads)
-        doc->addEntityTreeNodeSequence(taskData.seqTransferredEntity);
-    };
-    auto fnDispatchMessages = [=](TaskData& taskData) {
-        const auto strFilepath = taskData.filepath.make_preferred().u8string();
-        dispatchWarnings(
-            fmt::format("Warning(s) during import from '{}'", strFilepath),
-            taskData.messenger, messenger
-        );
-        dispatchErrors(
-            fmt::format("Errors(s) during import from '{}'", strFilepath),
-            taskData.messenger, messenger
-        );
-        taskData.messenger.clear();
-    };
-
-    if (listFilepath.size() == 1) { // Single file case
-        TaskData taskData;
-        taskData.filepath = listFilepath.front();
+    if (args.filepaths.size() == 1) {
+        // Single file case
+        ImportTaskData taskData;
+        taskData.filepath = args.filepaths.front();
         taskData.progress = rootProgress;
-        ok = fnReadFile(taskData);
-        if (ok) {
-            fnTransfer(taskData);
-            fnPostProcess(taskData);
-            fnAddModelTreeEntities(taskData);
-        }
-
-        fnDispatchMessages(taskData);
+        readFile(taskData, *this, args);
+        transfer(taskData, args);
+        postProcess(taskData, args);
+        addModelTreeEntities(taskData, args.targetDocument);
+        dispatchMessages(taskData, messenger);
+        return success(taskData);
     }
-    else { // Many files case
-        std::vector<TaskData> vecTaskData;
-        vecTaskData.resize(listFilepath.size());
+    else {
+        // Many files case
+        std::vector<ImportTaskData> vecTaskData;
+        vecTaskData.resize(args.filepaths.size());
 
         TaskManager childTaskManager;
         childTaskManager.signalProgressChanged.connectSlot([&](TaskId, double) {
@@ -327,38 +344,39 @@ bool System::importInDocument(const Args_ImportInDocument& args) const
         });
 
         // Read files
-        for (TaskData& taskData : vecTaskData) {
-            taskData.filepath = listFilepath[&taskData - &vecTaskData.front()];
+        for (ImportTaskData& taskData : vecTaskData) {
+            taskData.filepath = args.filepaths[&taskData - &vecTaskData.front()];
             taskData.taskId = childTaskManager.newTask([&](TaskProgress* progressChild) {
                 taskData.progress = progressChild;
-                taskData.readSuccess = fnReadFile(taskData);
+                readFile(taskData, *this, args);
             });
         }
 
-        for (const TaskData& taskData : vecTaskData)
+        for (const ImportTaskData& taskData : vecTaskData)
             childTaskManager.run(taskData.taskId, TaskAutoDestroy::Off);
 
-        // Transfer to document
+        // Transfer to target document
         auto taskDataCount = static_cast<int>(vecTaskData.size());
         while (taskDataCount > 0 && !rootProgress->isAbortRequested()) {
-            auto it = std::find_if(vecTaskData.begin(), vecTaskData.end(), [&](const TaskData& taskData) {
+            auto it = std::find_if(vecTaskData.begin(), vecTaskData.end(), [&](const ImportTaskData& taskData) {
                 return !taskData.transferred && childTaskManager.waitForDone(taskData.taskId, 25);
             });
-
             if (it != vecTaskData.end()) {
-                if (it->readSuccess) {
-                    fnTransfer(*it);
-                    fnPostProcess(*it);
-                    fnAddModelTreeEntities(*it);
-                }
-
-                fnDispatchMessages(*it);
+                transfer(*it, args);
+                postProcess(*it, args);
+                addModelTreeEntities(*it, args.targetDocument);
+                dispatchMessages(*it, messenger);
                 --taskDataCount;
             }
         } // endwhile
-    }
 
-    return ok;
+        for (const ImportTaskData& taskData : vecTaskData) {
+            if (!success(taskData))
+                return false;
+        }
+
+        return true;
+    }
 }
 
 System::Operation_ImportInDocument System::importInDocument() const
