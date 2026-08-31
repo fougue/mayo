855179b0c77f2ca5d499743ff4b27110491ee3a3
Hugues Delorme <delorme.hugues@fougue.pro>
2026-08-31 17:52:00 +0200

    Test_IO: add IO::ImageWriter tests

--- tests/test_io.h
+++ tests/test_io.h
@@ -14,26 +14,30 @@ namespace IO { class System; }
 class TestIO : public QObject {
     Q_OBJECT
 private slots:
+    void DxfReader_getPlainMText_test();
+    void DxfReader_getPlainMText_test_data();
+    void DxfReader_lwPolylineClosedDuplicateLastVertex_test();
+    void DxfReader_replaceTextControlCodes_test();
+    void DxfReader_replaceTextControlCodes_test_data();
+
+    void ImageWriter_emptyTransfer_test();
+    void ImageWriter_backgroundGradientFill_test();
+    void ImageWriter_backgroundGradientFill_test_data();
+    void ImageWriter_writeValidPngFile_test();
+    void ImageWriter_writeValidPngFile_test_data();
+
+    void OccStaticVariablesRollback_test();
+    void OccStaticVariablesRollback_test_data();
+
     void Regression_bugGitHub166_test();
     void Regression_bugGitHub166_test_data();
     void Regression_bugGitHub258_test();
     void Regression_bugGitHub332_test();
 
+    void System_importInDocument_catchVrmlReaderSendFail_test();
+    void System_probeFormatDirect_test();
     void System_probeFormat_test();
     void System_probeFormat_test_data();
-    void System_probeFormatDirect_test();
-    void System_importInDocument_catchVrmlReaderSendFail_test();
-
-    void OccStaticVariablesRollback_test();
-    void OccStaticVariablesRollback_test_data();
-
-    void DxfReader_replaceTextControlCodes_test();
-    void DxfReader_replaceTextControlCodes_test_data();
-
-    void DxfReader_getPlainMText_test();
-    void DxfReader_getPlainMText_test_data();
-
-    void DxfReader_lwPolylineClosedDuplicateLastVertex_test();
 
     void initTestCase();
     void cleanupTestCase();
