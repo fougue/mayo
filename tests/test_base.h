4d13274c51018ad3da15866d4f58c84f867bd12c
Hugues Delorme <delorme.hugues@fougue.pro>
2026-09-02 18:16:50 +0200

    Tests: move SignalEmitSpy into its own header/source files

--- tests/test_base.h
+++ tests/test_base.h
@@ -119,7 +119,8 @@ private slots:
     void UnitSystem_test();
     void UnitSystem_test_data();
 
-    void LibTask_test();
+    void LibTask_runAndTrackProgress_test();
+
     void LibTree_test();
     void LibTree_nodeRoot_test();
     void LibTree_removeRoot_test();
